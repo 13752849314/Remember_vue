fa5b535b64af9ff72c8ffe59282f98520491d4e5
happygh0st <ouao_happygh0st@163.com>
2024-04-20 00:09:14 +0800

    20240420

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="user">
-      <div class="search">
-          <el-button type="primary" @click="addRef.drawer=true">添加</el-button>
-      </div>
-    <el-table :data="users" style="width: 95%">
-      <el-table-column prop="id" label="编号"/>
-      <el-table-column prop="username" label="用户名"/>
-      <el-table-column prop="phone" label="电话"/>
-      <el-table-column prop="email" label="邮箱"/>
-      <el-table-column prop="birthday" label="生日"/>
-      <el-table-column prop="roles" label="角色"/>
-      <el-table-column prop="created_at" label="创建于"/>
-      <el-table-column label="操作">
-        <template #default="scope">
-          <el-button size="small" @click="handleEdit(scope.row)">
-            编辑
-          </el-button>
-          <el-button
-              size="small"
-              type="danger"
-              @click="handleDelete(scope.row.username)">
-            删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-      <Drawer ref="addRef"></Drawer>
-  </div>
+    <div class="user">
+        <div class="search">
+            <el-button type="primary" @click="addRef.drawer=true">添加</el-button>
+        </div>
+        <el-table :data="users" style="width: 95%">
+            <el-table-column prop="id" label="编号"/>
+            <el-table-column prop="username" label="用户名"/>
+            <el-table-column prop="phone" label="电话"/>
+            <el-table-column prop="email" label="邮箱"/>
+            <el-table-column prop="birthday" label="生日"/>
+            <el-table-column prop="roles" label="角色"/>
+            <el-table-column prop="created_at" label="创建于"/>
+            <el-table-column label="操作">
+                <template #default="scope">
+                    <el-button size="small" @click="handleEdit(scope.row)">
+                        编辑
+                    </el-button>
+                    <el-button
+                            size="small"
+                            type="danger"
+                            @click="handleDelete(scope.row.username)">
+                        删除
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <Drawer ref="addRef"></Drawer>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -36,21 +36,21 @@ import Drawer from "../../components/Drawer.vue"
 
 let users = ref([])
 let getAllUsers = () => {
-  let users1= $GetUsers()
-  users1.then(data => {
-    users.value = data.data.user
-  })
+    let users1 = $GetUsers()
+    users1.then(data => {
+        users.value = data.data.user
+    })
 }
 onMounted(() => {
-  getAllUsers()
+    getAllUsers()
 })
 
 const handleEdit = (row: any) => {
-  console.log(row)
+    console.log(row)
 }
 
 const handleDelete = (username: string) => {
-  console.log(username)
+    console.log(username)
 }
 
 let addRef = ref(null)
@@ -58,7 +58,7 @@ let addRef = ref(null)
 </script>
 
 <style scoped lang="scss">
-.search{
-  margin-bottom: 5px;
+.search {
+    margin-bottom: 5px;
 }
 </style>
